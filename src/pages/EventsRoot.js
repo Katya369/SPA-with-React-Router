95d19a6e026888b05716cf33b453b28c950be983
Ekaterina <nekrev@gmail.com>
2023-04-05 12:10:23 +0300

    Created

---
+++ src/pages/EventsRoot.js
@@ -0,0 +1,11 @@
+import EventsNavigation from '../components/EventsNavigation';
+import { Outlet } from 'react-router-dom';
+function EventsRoot() {
+return (
+<>
+    <EventsNavigation />
+    <main><Outlet /> </main>
+</>
+);
+};
+export default EventsRoot;
---
+++ src/pages/Home.js
@@ -0,0 +1,5 @@
+import PageContent from '../components/PageContent';
+function Home () {
+    return <PageContent title='Welcome'><p>Browse all our amaizing events!</p></PageContent>
+};
+export default Home;
